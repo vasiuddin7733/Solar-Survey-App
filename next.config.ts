b62be4336ee499436e547cbf028940728a471322
vasiuddin <vasiuddin.mohammed1@gmail.com>
2025-09-28 20:37:46 +0200

    update next comfig file

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
+  basePath: isProd ? "/Solar-Survey-App" : "",
+  assetPrefix: isProd ? "/Solar-Survey-App/" : "",
 };
 
 export default nextConfig;
