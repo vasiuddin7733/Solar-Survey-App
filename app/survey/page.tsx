2ca4bed149d9c5c726071cbf0d39dd27dfac436c
vasiuddin <vasiuddin.mohammed1@gmail.com>
2025-09-28 20:51:41 +0200

    add base path to the next config

--- app/survey/page.tsx
+++ app/survey/page.tsx
@@ -16,6 +16,8 @@ export type SurveyData = {
   phone?: string;
 };
 
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
 const Survey = () => {
   const router = useRouter();
   const [step, setStep] = useState(1);
@@ -44,7 +46,7 @@ const Survey = () => {
 
     setLoading(true);
     try {
-      const res = await fetch("/api/submit", {
+      const res = await fetch(`${basePath}/api/submit`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
@@ -52,7 +54,7 @@ const Survey = () => {
 
       const result = await res.json();
       console.log("Response data:", result);
-      router.push(`/result?answer=${result.answer}`);
+      router.push(`${basePath}/result?answer=${result.answer}`);
     } finally {
       setLoading(false);
     }
